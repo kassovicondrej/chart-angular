3b1da5c16b39f7e43018f3b3e5166aaaef197b18
Ondrej Kassovic <ondrej.kassovic@posam.sk>
2018-11-14 10:59:09 +0100

    zaokruhlenie grafov

--- chart/src/app/barchart/chart.component.ts
+++ chart/src/app/barchart/chart.component.ts
@@ -15,7 +15,7 @@ export class ChartComponent implements OnInit {
   @ViewChild(BaseChartDirective)
   public chart: BaseChartDirective;
 
-  public barChartType: String = 'bar';
+  public barChartType: String = 'roundedBar';
   public barChartLegend: Boolean = true;
   public barChartData: any[] = [
     { data: [20, 30, 60, 40, 50, 35, 25, 34, 44, 15, 28, 15, 5, 45], label: 'Uspesne', stack: '1', backgroundColor: '#18afac' },
@@ -27,7 +27,7 @@ export class ChartComponent implements OnInit {
 
   public barChartOptions: any = {
     responsive: true,
-    barRoundness: 10,
+    barRoundness: 1.1,
     legend: {
       display: true
     },
@@ -95,3 +95,108 @@ export class ChartComponent implements OnInit {
   }
 
 }
+
+
+// roudned bars
+declare var Chart: any;
+
+( Chart as any ).helpers.drawRoundedTopRectangle = function(ctx, x, y, width, height, radius) {
+  ctx.beginPath();
+  ctx.moveTo(x + radius, y);
+  ctx.lineTo(x + width - radius, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+  ctx.lineTo(x + width, y + height - radius);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+  ctx.lineTo(x + radius, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+  ctx.lineTo(x, y + radius);
+  ctx.quadraticCurveTo(x, y, x + radius, y);
+  ctx.closePath();
+};
+
+( Chart as any ).elements.RoundedTopRectangle = ( Chart as any ).elements.Rectangle.extend({
+  draw: function() {
+    const ctx = this._chart.ctx;
+    const vm = this._view;
+    let left, right, top, bottom, signX, signY, borderSkipped;
+    let borderWidth = vm.borderWidth;
+
+    if (!vm.horizontal) {
+      // bar
+      left = vm.x - vm.width / 2;
+      right = vm.x + vm.width / 2;
+      top = vm.y;
+      bottom = vm.base;
+      signX = 1;
+      signY = bottom > top? 1: -1;
+      borderSkipped = vm.borderSkipped || 'bottom';
+    } else {
+      // horizontal bar
+      left = vm.base;
+      right = vm.x;
+      top = vm.y - vm.height / 2;
+      bottom = vm.y + vm.height / 2;
+      signX = right > left? 1: -1;
+      signY = 1;
+      borderSkipped = vm.borderSkipped || 'left';
+  }
+
+  // Canvas doesn't allow us to stroke inside the width so we can
+  // adjust the sizes to fit if we're setting a stroke on the line
+  if (borderWidth) {
+    // borderWidth shold be less than bar width and bar height.
+    const barSize = Math.min(Math.abs(left - right), Math.abs(top - bottom));
+    borderWidth = borderWidth > barSize? barSize: borderWidth;
+    const halfStroke = borderWidth / 2;
+    // Adjust borderWidth when bar top position is near vm.base(zero).
+    const borderLeft = left + (borderSkipped !== 'left'? halfStroke * signX: 0);
+    const borderRight = right + (borderSkipped !== 'right'? -halfStroke * signX: 0);
+    const borderTop = top + (borderSkipped !== 'top'? halfStroke * signY: 0);
+    const borderBottom = bottom + (borderSkipped !== 'bottom'? -halfStroke * signY: 0);
+    // not become a vertical line?
+    if (borderLeft !== borderRight) {
+      top = borderTop;
+      bottom = borderBottom;
+    }
+    // not become a horizontal line?
+    if (borderTop !== borderBottom) {
+      left = borderLeft;
+      right = borderRight;
+    }
+  }
+
+  // calculate the bar width and roundess
+  const barWidth = Math.abs(left - right);
+  const roundness = this._chart.config.options.barRoundness || 0.5;
+  const radius = barWidth * roundness * 0.5;
+
+  // keep track of the original top of the bar
+  const prevTop = top;
+
+  // move the top down so there is room to draw the rounded top
+  top = prevTop + radius;
+  const barRadius = top - prevTop;
+
+  ctx.beginPath();
+  ctx.fillStyle = vm.backgroundColor;
+  ctx.strokeStyle = vm.borderColor;
+  ctx.lineWidth = borderWidth;
+
+  // draw the rounded top rectangle
+  ( Chart as any ).helpers.drawRoundedTopRectangle(ctx, left, (top - barRadius - 15), barWidth, bottom - prevTop + 15, barRadius );
+
+  ctx.fill();
+  if (borderWidth) {
+    ctx.stroke();
+  }
+
+  // restore the original top value so tooltips and scales still work
+  top = prevTop;
+},
+});
+
+( Chart as any ).defaults.roundedBar = ( Chart as any ).helpers.clone(( Chart as any ).defaults.bar);
+
+( Chart as any ).controllers.roundedBar = ( Chart as any ).controllers.bar.extend({
+  dataElementType: ( Chart as any ).elements.RoundedTopRectangle
+});
